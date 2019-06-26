83e6e7abaf88153905afa1af7fae4cd8703e7dd0
dac <dac@dypm.org>
2019-06-26 11:03:51 +0530

    first commit

---
+++ employee.js
@@ -0,0 +1,16 @@
+var express=require('express');
+var app=express();
+var employeeController=function(req,res)
+{
+var employee=[{Employeeid:1,Employeename:"Sameer",Salary:30000},{Employeeid:2,Employeename:"Ramesh",salary:50000},{}
+
+];
+res.send(employee)
+};
+app.get('/employee',employeeController);
+var server=app.listen(8086,function()
+{
+var host=server.address().address;
+var port=server.address().port;
+console.log("express listening at http://localhost:8086",host,port);
+})
