c1aa099c1253a5d6a71b59e2551235051b464add
dac <dac@dypm.org>
2019-06-26 11:50:53 +0530

    final commit

--- employee.js
+++ employee.js
@@ -8,9 +8,9 @@ var employee=[{Employeeid:1,Employeename:"Sameer",Salary:30000},{Employeeid:2,Em
 res.send(employee)
 };
 app.get('/employee',employeeController);
-var server=app.listen(8086,function()
+var server=app.listen(8000,function()
 {
 var host=server.address().address;
 var port=server.address().port;
-console.log("express listening at http://localhost:8086",host,port);
+console.log("express listening at http://localhost:8000",host,port);
 })
